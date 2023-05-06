ad090fd471e9bd8938e1a99e7bf2ad4818580b6b
Mohammed Shaikh <shaikh.adnan@hotmail.com>
2023-05-06 00:16:26 -0400

    added functionality to nav, minor html changes

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Get the button and add a click event listener to it
-    // when the button is clicked change the background of hero image to the stars one
-    // update the text so that it also matches with when the stars is also on or off
-
 const button = document.querySelector(".stars");
 
 const home = document.querySelector(".home")
@@ -17,6 +13,31 @@ button.addEventListener("click", displayStars);
         home.style.backgroundImage = `url('./assets/staticbackground.png')`
     }
 }
+
+// First, we need to go get the hamburger icon from the page (using .querySelector).
+const hamburger = document.querySelector(".hamburger");
+
+// Get a second item from the page being the UL of the main nav to manipulate with JS.
+const headerMain = document.querySelector(".headerMain");
+
+// Then, we attach an event listener to that icon that listens for the click event.
+hamburger.addEventListener("click", showMenu);
+
+
+function showMenu() {
+    headerMain.classList.toggle("headerMainActive");
+}
+
+const navRefresh = document.querySelectorAll("#nav");
+navRefresh.forEach(function(link){
+    link.addEventListener("click", unshowMenu)
+});
+
+function unshowMenu() {
+    console.log("clicked wow");
+}
+
+
 
 
 
